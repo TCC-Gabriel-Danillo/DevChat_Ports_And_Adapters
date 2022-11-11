d0bbd10441882f1b35298b5ee19121583adfd408
GabrielDvPereira <gdavi.gd@gmail.com>
2022-11-10 22:01:52 -0300

    refactor: add firebase secrets to .env

--- src/ui/app.config.js
+++ src/ui/app.config.js
@@ -1,6 +1,9 @@
-{
+import 'dotenv/config'
+
+export default {
   "expo": {
     "name": "Ui",
+    "scheme": "devchat.app",
     "slug": "Ui",
     "version": "1.0.0",
     "orientation": "portrait",
@@ -28,6 +31,17 @@
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    "extra": {
+      "git_cliet_secret": process.env.GIT_CLIENT_SECRET, 
+      "git_client_id": process.env.GIT_CLIENT_ID,
+      "firebase_api_key": process.env.FIREBASE_API_KEY,
+      "firebase_auth_domain": process.env.FIREBASE_AUTH_DOMAIN,
+      "firebase_project_id": process.env.FIREBASE_PROJECT_ID,
+      "firebase_storage_bucket": process.env.FIREBASE_STORAGE_BUCKET,
+      "firebase_messaging_sender_id": process.env.FIREBASE_MESSAGING_SENDER_ID,
+      "firebase_app_id": process.env.FIREBASE_APP_ID,
+      "firebase_measurement_id": process.env.FIREBASE_MEASUREMENT_ID,
     }
   }
 }
---
+++ src/ui/src/constants/secrets.ts
@@ -0,0 +1,12 @@
+import Constants from "expo-constants";
+
+export const GIT_CLIENT_SECRET = Constants?.manifest?.extra?.git_cliet_secret;
+export const GIT_CLIENT_ID = Constants?.manifest?.extra?.git_client_id;
+
+export const FIREBASE_API_KEY = Constants?.manifest?.extra?.firebase_api_key;
+export const FIREBASE_AUTH_DOMAIN = Constants?.manifest?.extra?.firebase_auth_domain;
+export const FIREBASE_PROJECT_ID = Constants?.manifest?.extra?.firebase_project_id;
+export const FIREBASE_STORAGE_BUCKET = Constants?.manifest?.extra?.firebase_storage_bucket;
+export const FIREBASE_MESSAGING_SENDER_ID = Constants?.manifest?.extra?.firebase_messaging_sender_id;
+export const FIREBASE_APP_ID = Constants?.manifest?.extra?.firebase_app_id;
+export const FIREBASE_MEASUREMENT_ID = Constants?.manifest?.extra?.firebase_measurement_id;

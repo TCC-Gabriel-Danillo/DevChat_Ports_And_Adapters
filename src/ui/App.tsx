39c982d1583104969905fe3e1bdcf1c1ffd079b5
GabrielDvPereira <gdavi.gd@gmail.com>
2022-11-14 10:02:17 -0300

    feat: add custom font

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -4,13 +4,17 @@ import { FirebaseDatabaseRepository, HttpRepositoryImp } from "@infrastructure/
 import {  AuthService } from "@domain/services"
 import { FIREBASE_COLLECTION, GITHUB_URL } from "./src/constants";
 import { Navigation } from "@ui/src/navigation"
+import { useCustomFonts } from "./src/hooks";
 
 export default function App() {
+  const [isFontLoaded] = useCustomFonts()
 
   const gitAuthHttp = new HttpRepositoryImp(GITHUB_URL.AUTH_BASE_URL)
   const gitApiHttp = new HttpRepositoryImp(GITHUB_URL.API_BASE_URL)
   const userDbRepository = new FirebaseDatabaseRepository(FIREBASE_COLLECTION.USERS)
   const authService = new AuthService(gitAuthHttp, gitApiHttp, userDbRepository)
+
+  if(!isFontLoaded) return
 
   return (
     <AuthContextProvider authService={ authService }>

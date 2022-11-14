a1316611fd1d5ce7e4e6a1fa04a1504badec77e3
GabrielDvPereira <gdavi.gd@gmail.com>
2022-11-14 10:02:40 -0300

    feat: add colors

--- src/ui/src/constants/index.ts
+++ src/ui/src/constants/index.ts
@@ -1,5 +1,7 @@
 export * from './secrets'
 export * from "./collections"
 export * from './urls'
+export * from './colors'
 export * from './screens/authScreens'
 export * from './screens/mainScreens'
+export * from './fonts'

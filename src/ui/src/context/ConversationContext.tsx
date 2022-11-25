5beff63311fc7181dd3b689669f89f02335fe64d
GabrielDvPereira <gdavi.gd@gmail.com>
2022-11-25 20:54:21 -0300

    feat: create conversation context

---
+++ src/ui/src/context/ConversationContext.tsx
@@ -0,0 +1,45 @@
+import { Conversation } from '@domain/entities/models'
+import { ConversationUseCase } from '@domain/entities/usecases'
+import { createContext, useState, useEffect, ReactNode } from 'react'
+import { useAuth } from '../hooks'
+
+
+interface ConversationContextInfo {
+    conversations: Conversation[]
+}
+
+export const ConversationContext = createContext<ConversationContextInfo>({} as ConversationContextInfo)
+
+interface Props {
+    children: ReactNode
+    conversationService: ConversationUseCase
+}
+
+export function CreateConversationContextProvider({ 
+    children,  
+    conversationService 
+}: Props){
+    
+    const { user } = useAuth()
+    const userId = user?.id
+
+    const [conversations, setConversations] = useState<Conversation[]>([])
+    
+    useEffect(() => {
+        getConversationsByUser()
+    }, [])
+
+    async function getConversationsByUser(){
+        if(!userId) return
+        const response = await conversationService.getConversationsByUserId(userId)
+        setConversations(response)
+    }
+
+    return(
+        <ConversationContext.Provider value={{ 
+            conversations 
+        }}>
+            {children}
+        </ConversationContext.Provider>
+    )
+}

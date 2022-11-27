23dd6c5f5432647f98a8cd6471f64b6cc7616889
GabrielDvPereira <gdavi.gd@gmail.com>
2022-11-27 19:38:12 -0300

    feat: add empty component

---
+++ src/ui/src/components/Empty/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    container: { 
+        flex: 1,
+        height: "100%", 
+        justifyContent: "center", 
+        alignItems: "center"
+    }, 
+    image: { width: 200, height: 200 }, 
+    message: { textAlign: "center" }
+})
+
+export default styles

14b77fe56b6f5f2f9a79865917e47f4019dd1dc8
GabrielDvPereira <gdavi.gd@gmail.com>
2022-11-27 19:41:55 -0300

    refactor: apply colors enum

--- src/ui/src/components/container/styles.ts
+++ src/ui/src/components/container/styles.ts
@@ -1,9 +1,9 @@
-import { LIGHT } from "@ui/src/constants"
+import { COLORS } from "@ui/src/constants"
 import { StyleSheet } from "react-native"
 
 export const styles = StyleSheet.create({
     container: {
-        backgroundColor: LIGHT, 
+        backgroundColor: COLORS.LIGHT, 
         padding: 32, 
         flex: 1, 
         width: '100%', 

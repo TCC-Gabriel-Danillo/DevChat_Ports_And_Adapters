cd5a0a8b3d21ba97dc885c3a9b68ec565b49c195
GabrielDvPereira <gdavi.gd@gmail.com>
2022-11-27 20:13:15 -0300

    feat: adjust home screen

--- src/ui/src/screens/HomeScreen/index.tsx
+++ src/ui/src/screens/HomeScreen/index.tsx
@@ -1,34 +1,50 @@
-import { Container, Button, Loading, AddButton } from '@ui/src/components'
-import { MAIN_SCREENS, TEST_ID } from '@ui/src/constants'
-import { useConversation, useMainNavigation } from '@ui/src/hooks'
+import { FlatList, TouchableOpacity } from "react-native"
+import { Container, Text, Loading, AddButton, UserCard } from '@ui/src/components'
+import { COLORS, ICONS, MAIN_SCREENS, TEST_ID } from '@ui/src/constants'
+import { useAuth, useConversation, useMainNavigation } from '@ui/src/hooks'
 import { styles } from './styles'
+import { User } from "@domain/entities/models"
 
 export function HomeScreen(){
     const { conversations, isLoadingConversations } = useConversation()
     const navigation = useMainNavigation()
-    
+    const { user } = useAuth()
+
     if(isLoadingConversations) return <Loading />
     
     const goToTechScreen = () => {
         navigation.navigate(MAIN_SCREENS.TECH_SCREEN); 
     }
+
+    const goToMessageScreen = (conversationId: string, participant: User) => {
+        navigation.navigate(MAIN_SCREENS.MESSAGE_SCREEN, { 
+            conversationId: conversationId,
+            participant: participant
+        })
+    }
     
     return(
-        <Container style={styles.container} testID={TEST_ID.HOME}>
-            {
-                conversations?.map(conversation => (
-                    <Button 
-                        key={conversation.id}
-                        onPress={() => {
-                            navigation.navigate(MAIN_SCREENS.MESSAGE_SCREEN, { 
-                                conversationId: conversation.id,
-                                participant: conversation.users[0] 
-                            })
-                    }}>
-                        {conversation.id}
-                    </Button>
-                ))
-            }
+        <Container testID={TEST_ID.HOME}>
+            <Text fontType="h2">Suas conversas iniciadas para cada tecnologia de interesse:</Text>
+            <FlatList 
+                data={conversations}
+                renderItem={({item: conversation}) => {
+                    const participant = conversation.users.filter(cUser => cUser.id !== user?.id)[0]
+                    return(
+                        <TouchableOpacity 
+                            onPress={() => goToMessageScreen(conversation.id, participant)} 
+                            style={styles.userCard}
+                        >
+                            <UserCard 
+                                photoUrl={participant.photoUrl}
+                                title={participant.username}
+                                subtile={`Tema: ${conversation.tech}`}
+                            />
+                            <ICONS.CARRET_RIGHT  color={COLORS.GREY}/>
+                        </TouchableOpacity>
+                    )
+                }}
+            />
             <AddButton style={styles.addButton} onPress={goToTechScreen} />
         </Container>
     )

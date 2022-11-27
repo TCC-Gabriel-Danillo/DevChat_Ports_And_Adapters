c9a18ad8f0ddba1fd60c0e86b291764f24a8031e
GabrielDvPereira <gdavi.gd@gmail.com>
2022-11-27 16:27:45 -0300

    feat: add flatlist to tech list

--- src/ui/src/screens/TechsScreen/index.tsx
+++ src/ui/src/screens/TechsScreen/index.tsx
@@ -3,25 +3,28 @@ import { COLORS } from "@ui/src/constants";
 import { useAuth } from "@ui/src/hooks";
 import { ICONS } from "@ui/src/constants"
 import styles from "./styles"
+import { FlatList } from "react-native";
 
 export function TechScreen(){
     const { user } = useAuth()
 
-    const rederCardList = () => {
-        return user?.techs?.map(tech =>( 
-            <Card 
-              icon={<ICONS.CARRET_RIGHT color={COLORS.GREY} />}
-              style={styles.card}
-             >
-              <Text style={styles.tech}>{tech}</Text>        
-            </Card>
-          )) 
-    }
-
     return(
         <Container>
             <Text fontType="h2" style={styles.title}>Escolha a tecnologia para iniciar sua conversa: </Text>
-            {rederCardList()}
+            <FlatList 
+                data={user?.techs}
+                renderItem={({item: tech}) => {
+                    return( 
+                        <Card 
+                            key={tech}
+                            icon={<ICONS.CARRET_RIGHT color={COLORS.GREY} />}
+                            style={styles.card}
+                        >
+                            <Text style={styles.tech}>{tech}</Text>        
+                        </Card>
+                    )
+                }}
+            />
         </Container>
     )
 }

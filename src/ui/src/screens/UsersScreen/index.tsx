dad6f2ddaf4a8eed646fa161625f67994a0d959e
GabrielDvPereira <gdavi.gd@gmail.com>
2022-11-27 19:56:30 -0300

    feat: add users screen

--- src/ui/src/screens/UsersScreen/index.tsx
+++ src/ui/src/screens/UsersScreen/index.tsx
@@ -3,10 +3,13 @@ import { Text, Loading } from "@ui/src/components"
 import { useUsers } from "@ui/src/hooks/useUsers";
 import { useRoute } from "@react-navigation/native"
 import { NavOpts } from "@ui/src/navigation/main";
-import { MAIN_SCREENS, SCREEN_WIDTH } from "@ui/src/constants";
-import { RouteProp } from "@react-navigation/native"
+import { MAIN_SCREENS } from "@ui/src/constants";
+import { RouteProp } from "@react-navigation/native";
 import { useEffect } from "react";
-import { FlatList, View, Image } from "react-native";
+import { FlatList } from "react-native";
+import { Empty, UserCard } from "@ui/src/components";
+import { parseArrayToString } from "@ui/src/utils";
+import styles from "./styles"
 
 export function UsersScreen(){
     const { users, getUsersByTech, isLoadingUsers } = useUsers()
@@ -18,26 +21,33 @@ export function UsersScreen(){
 
     if(isLoadingUsers) return <Loading />
 
+    const renderUserList = () => {
+        return (
+            <FlatList 
+                data={users}
+                renderItem={({item: user}) => {
+                    return(
+                        <UserCard 
+                            style={styles.userCard}
+                            photoUrl={user.photoUrl}
+                            title={user.username}
+                            subtile={parseArrayToString(user.techs, { limit: 5, separator:", "})}
+                        /> 
+                    )
+                }}
+            />
+        )
+    }
+
     return(
         <Container>
             <Text>
                 Usuários interessados na tecnologia <Text fontWeight="bold" >{params.tech}:</Text>
             </Text>
-            <FlatList 
-                ListEmptyComponent={<Text>Não há usuários interessados nessa tecnologia.</Text>}
-                data={users}
-                renderItem={({item: user}) => {
-                    return(
-                        <View style={{ flexDirection: 'row', marginTop: 20, alignItems: "center", width: SCREEN_WIDTH * 0.7 }}>
-                            <Image source={{ uri: user.photoUrl }} style={{ width: 60, height: 60, borderRadius: 60 }}/>
-                            <View style={{ marginLeft: 10 }}>
-                                <Text fontWeight="bold">{user.username}</Text>
-                                <Text style={{ fontSize: 14 }}>{user.techs?.slice(0,5).join(", ") + "..."}</Text>
-                            </View>
-                        </View>
-                    )
-                }}
-            />
+           {users.length ? 
+                renderUserList() : 
+                <Empty message="Não há usuários interessados nessa tecnologia."/>
+            }
         </Container>
     )    
 }

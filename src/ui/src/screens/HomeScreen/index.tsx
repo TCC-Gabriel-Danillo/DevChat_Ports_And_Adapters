7d766b7db3f99734c64658238d5d1cea80b00a50
GabrielDvPereira <gdavi.gd@gmail.com>
2022-12-02 11:32:44 -0300

    feat: add empty component to initial page

--- src/ui/src/screens/HomeScreen/index.tsx
+++ src/ui/src/screens/HomeScreen/index.tsx
@@ -1,5 +1,5 @@
-import { FlatList, TouchableOpacity } from "react-native"
-import { Container, Text, Loading, AddButton, UserCard, Badge } from '@ui/src/components'
+import { FlatList, TouchableOpacity, View } from "react-native"
+import { Container, Text, Loading, AddButton, UserCard, Badge, Empty } from '@ui/src/components'
 import { COLORS, ICONS, MAIN_SCREENS, TEST_ID } from '@ui/src/constants'
 import { useAuth, useConversation, useMainNavigation } from '@ui/src/hooks'
 import { styles } from './styles'
@@ -23,37 +23,51 @@ export function HomeScreen() {
         })
     }
 
+    const renderEmptyComponent = () => {
+        return (
+            <View style={styles.empty}>
+                <Empty />
+            </View>
+        )
+    }
+
+    const renderConversationList = () => {
+        return <FlatList
+            data={conversations}
+            renderItem={({ item: conversation }) => {
+
+                const participant = conversation.users.filter(cUser => cUser.id !== authUser?.id)[0]
+                const shouldShowBadge = conversation.unreadNumber > 0 && conversation.lastSenderId !== authUser?.id
+
+                return (
+                    <TouchableOpacity
+                        onPress={() => goToMessageScreen(conversation, participant)}
+                        style={styles.userCard}
+                        testID={TEST_ID.USER_CARD}
+                    >
+                        <UserCard
+                            photoUrl={participant.photoUrl}
+                            title={participant.username}
+                            subtile={`Tema: ${conversation.tech}`}
+                        />
+
+                        {shouldShowBadge && <Badge
+                            text={conversation.unreadNumber}
+                            style={styles.unreadNumber}
+                        />}
+                        <ICONS.CARRET_RIGHT color={COLORS.GREY} />
+                    </TouchableOpacity>
+                )
+            }}
+        />
+    }
+
     return (
         <Container testID={TEST_ID.HOME}>
             <Text fontType="h2">Suas conversas iniciadas para cada tecnologia de interesse:</Text>
-            <FlatList
-                data={conversations}
-                renderItem={({ item: conversation }) => {
-
-                    const participant = conversation.users.filter(cUser => cUser.id !== authUser?.id)[0]
-                    const shouldShowBadge = conversation.unreadNumber > 0 && conversation.lastSenderId !== authUser?.id
-
-                    return (
-                        <TouchableOpacity
-                            onPress={() => goToMessageScreen(conversation, participant)}
-                            style={styles.userCard}
-                            testID={TEST_ID.USER_CARD}
-                        >
-                            <UserCard
-                                photoUrl={participant.photoUrl}
-                                title={participant.username}
-                                subtile={`Tema: ${conversation.tech}`}
-                            />
-
-                            {shouldShowBadge && <Badge
-                                text={conversation.unreadNumber}
-                                style={styles.unreadNumber}
-                            />}
-                            <ICONS.CARRET_RIGHT color={COLORS.GREY} />
-                        </TouchableOpacity>
-                    )
-                }}
-            />
+            {conversations?.length ?
+                renderConversationList() :
+                renderEmptyComponent()}
             <AddButton testID={TEST_ID.ADD_BUTTON} style={styles.addButton} onPress={goToTechScreen} />
         </Container>
     )
--- src/ui/src/screens/HomeScreen/styles.ts
+++ src/ui/src/screens/HomeScreen/styles.ts
@@ -2,15 +2,16 @@ import { StyleSheet } from "react-native"
 
 export const styles = StyleSheet.create({
     addButton: {
-        position: "absolute", 
-        bottom: 30, 
+        position: "absolute",
+        bottom: 30,
         right: 30
-    }, 
-    userCard: { 
-        marginTop: 20, 
-        flexDirection: "row", 
-        alignItems: "center", 
+    },
+    userCard: {
+        marginTop: 20,
+        flexDirection: "row",
+        alignItems: "center",
         justifyContent: "space-between"
-    }, 
-    unreadNumber: { marginRight: 5 }
+    },
+    unreadNumber: { marginRight: 5 },
+    empty: { flex: 1, justifyContent: "center", alignItems: "center" }
 })

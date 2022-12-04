43abaa449977b914bd7f1110475f842df414212d
GabrielDvPereira <gdavi.gd@gmail.com>
2022-12-04 16:43:58 -0300

    chore: add codecov and actions

--- src/ui/src/screens/UsersScreen/index.tsx
+++ src/ui/src/screens/UsersScreen/index.tsx
@@ -20,6 +20,8 @@ export function UsersScreen() {
     useEffect(() => {
         getUsersByTech(params.tech)
     }, [])
+
+
     const renderUserList = () => {
         return (
             <FlatList
